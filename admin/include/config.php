a57217ea9124dc83eb9a8fc368a4106913df5607
sajmon91 <stefan.sajmon@gmail.com>
2022-06-21 10:52:35 +0200

    Update config.php

--- admin/include/config.php
+++ admin/include/config.php
@@ -9,10 +9,10 @@ if($_SERVER['SERVER_NAME'] == "localhost")
     define('DB_PASS', '');
     define('DB_NAME', 'liquorstore');
 }else{
-    define('DB_HOST', '');
-    define('DB_USER', '');
-    define('DB_PASS', '');
-    define('DB_NAME', '');
+    define('DB_HOST', 'sql11.freemysqlhosting.net');
+    define('DB_USER', 'sql11501168');
+    define('DB_PASS', 'CAT6Dr8t8N');
+    define('DB_NAME', 'sql11501168');
 }
 
 
