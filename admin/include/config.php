8d9fbd7de290910101c6e4adaea5e1ed79baab3e
sajmon91 <stefan.sajmon@gmail.com>
2022-06-21 10:26:30 +0200

    Revert "Update config.php"
    This reverts commit 2dba3da7623377f57a801a05c5178c79258cbaa7.

--- admin/include/config.php
+++ admin/include/config.php
@@ -2,18 +2,18 @@
 
 
 // DB params
-if($_SERVER['SERVER_NAME'] == "localhost")
-{
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'liquorstore');
-}else{
-    define('DB_HOST', '');
-    define('DB_USER', '');
-    define('DB_PASS', '');
-    define('DB_NAME', '');
-}
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'liquorstore');
+
+
+
+
+
+
+
+
 
 
 
